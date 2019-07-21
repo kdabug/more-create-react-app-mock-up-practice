696ef122d57b8b399020c0bf27f57ff9e5900dfa
Mikayda Mills <mills.ma12@gmail.com>
2019-07-21 19:43:12 -0400

    styling changes

--- src/data/bookList.js
+++ src/data/bookList.js
@@ -7,21 +7,25 @@ export const bookList = [
   {
     title: "Hamlet",
     image: hamletCover,
-    description: "Meanwhile back at Elsinore, Ophelia has gone mad with grief."
+    description:
+      "The Lion King. Prince Hamlet devotes himself to avenging his father’s death..Hamlet goes to confront his mother. Hamlet believes the king is hiding there. He draws his sword and stabs through the fabric, killing Polonius. For this crime, he is immediately dispatched to England with Rosencrantz and Guildenstern.. Meanwhile back at Elsinore, Ophelia has gone mad with grief. Everybody dies."
   },
   {
     title: "King Lear",
     image: kingLearCover,
-    description: "Meanwhile back at Elsinore, Ophelia has gone mad with grief."
+    description:
+      "King Lear. A Thousand Acres. Unable to believe that his beloved daughters are betraying him, Lear slowly goes insane. The despairing Gloucester tries to commit suicide, but Edgar saves him by pulling the strange trick of leading him off an imaginary cliff. Everyone dies."
   },
   {
     title: "Twelfth Night Cover",
     image: twelfthNightCover,
-    description: "Meanwhile back at Elsinore, Ophelia has gone mad with grief."
+    description:
+      "She's the Man. Duke Orsino of Illyria is in love with Olivia, but his advances are rejected. A shipwrecked Viola arrives on his shores, and with the help of a Captain, disguises herself as a boy, calling herself Cesario, and enters Orsino’s service. Orsino takes to Cesario, and sends ‘him’ to woo Olivia for him. Viola, however, is already falling in love with Orsino.Cesario arrives to woo Olivia, and Olivia falls in love with ‘him’. Malvolio is weird but everyone gets all up and married."
   },
   {
     title: "Romeo & Juliet",
     image: romeoJulietCover,
-    description: "Meanwhile back at Elsinore, Ophelia has gone mad with grief."
+    description:
+      "West Side Story. Paris goes to Juliet’s tomb to mourn her, and encounters Romeo. They fight, and Romeo kills Paris. Romeo then drinks the poison and dies by Juliet. The Friar arrives to see Romeo dead and Juliet waking. She refuses to leave, and kills herself with Romeo’s dagger."
   }
 ];
