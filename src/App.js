3eef1f197f14fec5fc209b0a37ac3a1232b10fd4
Mikayda Mills <mills.ma12@gmail.com>
2019-07-21 18:36:28 -0400

    structure of App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,65 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const bookList = [
+    {
+      title: "Hamlet",
+      image: "./assets/hamlet-cover.jpg",
+      description:
+        "Meanwhile back at Elsinore, Ophelia has gone mad with grief."
+    },
+    {
+      title: "Hamlet",
+      image: "./assets/hamlet-cover.jpg",
+      description:
+        "Meanwhile back at Elsinore, Ophelia has gone mad with grief."
+    },
+    {
+      title: "Hamlet",
+      image: "./assets/hamlet-cover.jpg",
+      description:
+        "Meanwhile back at Elsinore, Ophelia has gone mad with grief."
+    },
+    {
+      title: "Hamlet",
+      image: "./assets/hamlet-cover.jpg",
+      description:
+        "Meanwhile back at Elsinore, Ophelia has gone mad with grief."
+    }
+  ];
   return (
     <div className="App">
-      <main />
+      <main>
+        <Header />
+        {bookList.map(book => (
+          <Book
+            title={book.title}
+            image={book.image}
+            description={book.description}
+          />
+        ))}
+        {/* <Book
+          title={bookList[0].title}
+          image={book.image}
+          description={book.description}
+        />
+        <Book
+          title={bookList[0].title}
+          image={book.image}
+          description={book.description}
+        />
+        <Book
+          title={bookList[0].title}
+          image={book.image}
+          description={book.description}
+        />
+        <Book
+          title={bookList[0].title}
+          image={book.image}
+          description={book.description}
+        /> */}
+        <Footer />
+      </main>
     </div>
   );
 }
