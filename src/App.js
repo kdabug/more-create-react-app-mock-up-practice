f82c9040baf23b26a80a478852ed822dd6fa1fe8
Mikayda Mills <mills.ma12@gmail.com>
2019-07-21 19:47:43 -0400

    new solution no map branch

--- src/App.js
+++ src/App.js
@@ -10,33 +10,26 @@ function App() {
     <div className="App">
       <main>
         <Header />
-        {bookList.map(book => (
-          <Book
-            title={book.title}
-            image={book.image}
-            description={book.description}
-          />
-        ))}
-        {/* <Book
+        <Book
           title={bookList[0].title}
-          image={book.image}
-          description={book.description}
+          image={bookList[0].image}
+          description={bookList[0].description}
         />
         <Book
           title={bookList[1].title}
-          image={book.image}
-          description={book.description}
+          image={bookList[2].image}
+          description={bookList[2].description}
         />
         <Book
           title={bookList[2].title}
-          image={book.image}
-          description={book.description}
+          image={bookList[2].image}
+          description={bookList[2].description}
         />
         <Book
           title={bookList[3].title}
-          image={book.image}
-          description={book.description}
-        /> */}
+          image={bookList[3].image}
+          description={bookList[3].description}
+        />
         <Footer />
       </main>
     </div>
